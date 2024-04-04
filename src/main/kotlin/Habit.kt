8f7b9d3abb25d285480341804f035489fb870d20
Tim Mortimer <tim@kiteframe.co.uk>
2024-04-04 15:16:13 +0100

    Refactor to start a habit through a function

--- src/main/kotlin/Habit.kt
+++ src/main/kotlin/Habit.kt
@@ -3,17 +3,27 @@ package uk.co.kiteframe.habitpal
 import java.time.LocalDate
 import java.util.*
 
+fun startHabit(id: String, name: String, habitType: HabitType, startedOn: LocalDate): Habit {
+    return Habit(HabitId.of(id), NonBlankString(name), habitType, startedOn)
+}
+
 class Habit(val id: HabitId, val name: NonBlankString, val type: HabitType, val startedOn: LocalDate)
 
 sealed interface HabitType
 data object Daily : HabitType
 
 @JvmInline
-value class HabitId(val value: UUID)
+value class HabitId(val value: UUID) {
+    companion object {
+        fun of(uuid: String) = HabitId(UUID.fromString(uuid))
+    }
+}
 
 @JvmInline
 value class NonBlankString(val value: String) {
     init {
         require(value.trim().isNotEmpty()) { "Habit name cannot be blank" }
     }
+
+    override fun toString(): String = value
 }
--- src/test/kotlin/HabitTests.kt
+++ src/test/kotlin/HabitTests.kt
@@ -6,15 +6,15 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class HabitTests {
-    private val someHabitId = HabitId(UUID.randomUUID())
-    private val someName = NonBlankString("journal")
+    private val someUuid = "f47ac10b-58cc-4372-a567-0e02b2c3d479"
+    private val someName = "journal"
     private val someDate = LocalDate.of(2024, 3, 4)
 
     @Test
     fun `starting a habit`() {
-        val habit = Habit(someHabitId, someName, Daily, someDate)
-        assertEquals(someHabitId, habit.id)
-        assertEquals(someName, habit.name)
+        val habit: Habit = startHabit(someUuid, someName, Daily, someDate)
+        assertEquals(HabitId(UUID.fromString(someUuid)), habit.id)
+        assertEquals("journal", habit.name.toString())
         assertEquals(Daily, habit.type)
         assertEquals(someDate, habit.startedOn)
     }
