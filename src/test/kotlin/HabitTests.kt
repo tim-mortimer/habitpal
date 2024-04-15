ba55794b659986d4bb024d12a45810ab317acc97
Tim Mortimer <tim@kiteframe.co.uk>
2024-04-15 16:46:51 +0100

    Create extension functions for assertions

--- src/test/kotlin/HabitTests.kt
+++ src/test/kotlin/HabitTests.kt
@@ -1,9 +1,14 @@
 package uk.co.kiteframe.habitpal
 
+import arrow.core.Either
+import arrow.core.getOrElse
+import arrow.core.left
 import java.time.LocalDate
 import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.fail
 
 class HabitTests {
     private val someUuid = "f47ac10b-58cc-4372-a567-0e02b2c3d479"
@@ -14,44 +19,47 @@ class HabitTests {
     @Test
     fun `starting a habit`() {
         startHabit(someUuid, someName, someHabitType, someDate)
-            .getOrNone()
-            .map {
-                with(it) {
-                    assertEquals(HabitId(UUID.fromString(someUuid)), id)
-                    assertEquals("journal", name.toString())
-                    assertEquals(Daily, type)
-                    assertEquals(someDate, startedOn)
-                }
+            .shouldYieldHabitWith {
+                assertEquals(HabitId(UUID.fromString(someUuid)), id)
+                assertEquals("journal", name.toString())
+                assertEquals(Daily, type)
+                assertEquals(someDate, startedOn)
             }
     }
 
     @Test
     fun `starting a habit performed multiple time a day`() {
         startHabit(someUuid, someName, HabitType.MULTIPLE_TIMES_A_DAY, someDate, times = 2)
-            .getOrNone()
-            .map {
-                assertEquals(MultipleTimesADay(2), it.type)
+            .shouldYieldHabitWith {
+                assertEquals(MultipleTimesADay(2), type)
             }
     }
 
     @Test
     fun `cannot start a habit with an invalid UUID`() {
-        val result = startHabit("blah", someName, someHabitType, someDate)
-        assertEquals(IdIsNotAUuid, result.leftOrNull())
+        startHabit("blah", someName, someHabitType, someDate).shouldFailWith(IdIsNotAUuid)
     }
 
     @Test
     fun `cannot start a habit with a blank name`() {
-        assertEquals(BlankName, startHabit(someUuid, "", someHabitType, someDate).leftOrNull())
-        assertEquals(BlankName, startHabit(someUuid, " ", someHabitType, someDate).leftOrNull())
+        startHabit(someUuid, "", someHabitType, someDate).shouldFailWith(BlankName)
+        startHabit(someUuid, " ", someHabitType, someDate).shouldFailWith(BlankName)
     }
 
     @Test
     fun `habit names are trimmed`() {
         startHabit(someUuid, "journal ", someHabitType, someDate)
-            .getOrNone()
-            .map {
-                assertEquals("journal", it.name.toString())
+            .shouldYieldHabitWith {
+                assertEquals("journal", name.toString())
             }
+    }
+
+    private fun Either<StartHabitError, Habit>.shouldYieldHabitWith(assertions: Habit.() -> Unit) {
+        assertions.invoke(this.getOrElse { fail("Starting habit failed with ${this.left()}") })
+    }
+
+    private fun Either<StartHabitError, Habit>.shouldFailWith(error: StartHabitError) {
+        assertTrue { this.isLeft() }
+        assertEquals(error, this.leftOrNull())
     }
 }
