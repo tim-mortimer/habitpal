e40d65f53f4a7013ddf95504b72f61f2b1b6bc07
Tim Mortimer <tim@kiteframe.co.uk>
2024-07-30 10:30:12 +0100

    Add basic logging configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,9 @@ dependencies {
     implementation("org.http4k:http4k-template-handlebars")
     implementation("org.http4k:http4k-testing-approval")
     implementation("io.arrow-kt:arrow-core:1.2.1")
+    implementation("org.slf4j:slf4j-api:2.0.13")
+    implementation("ch.qos.logback:logback-core:1.5.6")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("io.kotest:kotest-assertions-core:5.9.0")
 }
