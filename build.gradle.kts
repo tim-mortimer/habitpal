ab93ad2d092b267c8b74a05a94d34439a7d3a28c
Tim Mortimer <tim@kiteframe.co.uk>
2024-08-18 21:22:26 +0100

    Remove redundant Jackson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ dependencies {
     implementation(platform("org.http4k:http4k-bom:5.20.0.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-client-apache")
-    implementation("org.http4k:http4k-format-jackson")
     implementation("org.http4k:http4k-htmx")
     implementation("org.http4k:http4k-server-undertow")
     implementation("org.http4k:http4k-serverless-lambda")
