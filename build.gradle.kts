383d6369212d38d6870b154d5613870ce7d4f079
Tim Mortimer <tim@kiteframe.co.uk>
2024-08-18 21:26:59 +0100

    Correctly declare test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,16 +12,16 @@ repositories {
 dependencies {
     implementation(platform("org.http4k:http4k-bom:5.20.0.0"))
     implementation("org.http4k:http4k-core")
-    implementation("org.http4k:http4k-client-apache")
     implementation("org.http4k:http4k-htmx")
     implementation("org.http4k:http4k-server-undertow")
     implementation("org.http4k:http4k-serverless-lambda")
     implementation("org.http4k:http4k-template-handlebars")
-    implementation("org.http4k:http4k-testing-approval")
     implementation("io.arrow-kt:arrow-core:1.2.1")
     implementation("org.slf4j:slf4j-api:2.0.13")
     implementation("ch.qos.logback:logback-core:1.5.6")
     implementation("ch.qos.logback:logback-classic:1.5.6")
+    testImplementation("org.http4k:http4k-client-apache")
+    testImplementation("org.http4k:http4k-testing-approval")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("io.kotest:kotest-assertions-core:5.9.0")
 }
