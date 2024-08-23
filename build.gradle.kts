89828a0f8d79c5e09217c25e3ccfef35efe4f75b
Tim Mortimer <tim@kiteframe.co.uk>
2024-08-23 17:38:38 +0100

    Set up jOOQ with code generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("org.jooq.jooq-codegen-gradle") version "3.19.11"
 }
 
 group = "uk.co.kiteframe.habitpal"
 version = "1.0-SNAPSHOT"
+
+val jooqVersion = "3.19.11"
 
 repositories {
     mavenCentral()
@@ -20,6 +23,13 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.13")
     implementation("ch.qos.logback:logback-core:1.5.6")
     implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("org.postgresql:postgresql:42.7.4")
+
+    jooqCodegen("org.postgresql:postgresql:42.7.4")
+    implementation("org.jooq:jooq:${jooqVersion}")
+    compileOnly("org.jooq:jooq-meta:${jooqVersion}")
+    compileOnly("org.jooq:jooq-codegen:${jooqVersion}")
+
     testImplementation("org.http4k:http4k-client-apache")
     testImplementation("org.http4k:http4k-testing-approval")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
@@ -40,4 +50,30 @@ tasks.test {
 
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.named("compileKotlin") {
+    dependsOn(tasks.named("jooqCodegen"))
+}
+
+jooq {
+    configuration {
+        jdbc {
+            driver = "org.postgresql.Driver"
+            url = "jdbc:postgresql://localhost:5432/habitpal"
+            user = "habitpal"
+            password = "habitpal"
+        }
+
+        generator {
+            name = "org.jooq.codegen.KotlinGenerator"
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                inputSchema = "public"
+            }
+            target {
+                packageName = "uk.co.kiteframe.habitpal.db"
+            }
+        }
+    }
 }
